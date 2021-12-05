f73e07b2e9bfbf2adefe0020a740f6e2bf9be0e2
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2021-12-05 18:49:54 +0300

    structure edited

---
+++ screenshot.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const Take = {
+      screenshot: async (url, width, height, folder_path, image_name, image_extension) => {
+        try {
+            const browser = await puppeteer.launch({
+                headless: false,
+                slowMo: 1000,
+                devtools: true
+            });
+            const page = await browser.newPage();
+            if( width && height ) {
+                await page.setViewport({ width, height });
+            }
+            let folderPath = folder_path ? folder_path : './screenshots'
+            await page.goto(url);
+            if (!fs.existsSync(`${folderPath}`)) {
+                fs.mkdirSync(`${folderPath}`)
+            }
+            let extension = image_extension ? image_extension : 'png'
+            let name = image_name ? image_name : `${Date.now()}.${extension}`
+            await page.screenshot({ path:`${folderPath}/${name}` });
+        }catch (e) {
+            console.log(e)
+        }
+    }
+
+}
+module.exports = Take
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require("body-parser");
-const puppeteer = require("puppeteer");
 const app = express()
 
-const fs = require('fs');
-
+const take = require('./screenshot')
 
 app.post('/', bodyParser.json(), async function (req, res) {
     if (!req.body.url) return res.status(401).json({status: 'error' , message: 'Url is required.'})
-    await screenShot(req.body.url, req.body.width, req.body.height, req.body.folder_path, req.body.image_name, req.body.image_extension)
+    await take.screenshot(req.body.url, req.body.width, req.body.height, req.body.folder_path, req.body.image_name, req.body.image_extension)
     return res.status(200).json({ status: 'success' })
 })
 
@@ -17,26 +15,3 @@ app.listen(process.env.POST, `${process.env.HOST}`,function () {
     console.log(`Server is running on PORT=${process.env.POST}`)
 })
 
-async function screenShot (url, width, height, folder_path, image_name, image_extension) {
-    try {
-        const browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 1000,
-            devtools: true
-        });
-        const page = await browser.newPage();
-        if( width && height ) {
-            await page.setViewport({ width, height });
-        }
-        let folderPath = folder_path ? folder_path : './screenshots'
-        await page.goto(url);
-        if (!fs.existsSync(`${folderPath}`)) {
-            fs.mkdirSync(`${folderPath}`)
-        }
-        let extension = image_extension ? image_extension : 'png'
-        let name = image_name ? image_name : `${Date.now()}.${extension}`
-        await page.screenshot({ path:`${folderPath}/${name}` });
-    }catch (e) {
-        console.log(e)
-    }
-}
