bd96835e3644502b893086d23b9e5d788f803def
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2021-12-12 11:09:29 +0300

    some conditions added.

--- screenshot.js
+++ screenshot.js
@@ -1,29 +1,38 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const Take = {
-      screenshot: async (url, width, height, folder_path, image_name, image_extension) => {
+      screenshot: async (url, width, height, image_name, image_extension, browser_close) => {
         try {
             const browser = await puppeteer.launch({
                 headless: false,
-                slowMo: 1000,
+                slowMo: 300,
                 devtools: true
             });
             const page = await browser.newPage();
+            if(width && height === undefined) {
+                await page.setViewport({ width, height: width });
+            }
+            if(width === undefined && height) {
+                await page.setViewport({ width: height, height });
+            }
             if( width && height ) {
                 await page.setViewport({ width, height });
             }
-            let folderPath = folder_path ? folder_path : `${__dirname}/screenshots`
+            let folderPath = `${__dirname}/screenshots`
             await page.goto(url);
             if (!fs.existsSync(`${folderPath}`)) {
                 fs.mkdirSync(`${folderPath}`)
             }
             let extension = image_extension ? image_extension : 'png'
-            let name = image_name ? image_name : `${Date.now()}.${extension}`
+            let name = image_name ? `${image_name}.${extension}` : `${Date.now()}.${extension}`
             await page.screenshot({ path:`${folderPath}/${name}` });
+            let closeTrigger = browser_close ? browser_close : true
+            if(closeTrigger) {
+                await browser.close();
+            }
         }catch (e) {
             console.log(e)
         }
     }
-
 }
 module.exports = Take
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const take = require('./screenshot')
 
 app.post('/', bodyParser.json(), async function (req, res) {
     if (!req.body.url) return res.status(401).json({status: 'error' , message: 'Url is required.'})
-    await take.screenshot(req.body.url, req.body.width, req.body.height, req.body.folder_path, req.body.image_name, req.body.image_extension)
+    await take.screenshot(req.body.url, req.body.width, req.body.height, req.body.image_name, req.body.image_extension, req.body.browser_close)
     return res.status(200).json({ status: 'success' })
 })
 
