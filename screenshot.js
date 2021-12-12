1bc20445e4f5fa60aad8a16c65a98f5453edf55f
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2021-12-12 14:44:29 +0300

    response data added.

--- screenshot.js
+++ screenshot.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 const Take = {
       screenshot: async (url, width, height, image_name, image_extension, browser_close) => {
         try {
+            let result = {
+                screen_info: {}
+            }
+            let ss_url = url.includes('://') ? url : 'https://' + url
             const browser = await puppeteer.launch({
                 headless: false,
                 slowMo: 300,
@@ -11,27 +15,43 @@ const Take = {
             const page = await browser.newPage();
             if(width && height === undefined) {
                 await page.setViewport({ width, height: width });
+                result.screen_info.width = width
+                result.screen_info.height = width
             }
             if(width === undefined && height) {
                 await page.setViewport({ width: height, height });
-            }
+                result.screen_info.width = height
+                result.screen_info.height = height            }
             if( width && height ) {
                 await page.setViewport({ width, height });
+                result.screen_info.width = width
+                result.screen_info.height = height
             }
             let folderPath = `${__dirname}/screenshots`
-            await page.goto(url);
+            result.folder_path = `${__dirname}/screenshots`
+            await page.goto(ss_url);
+            result.url = `${ss_url}`
             if (!fs.existsSync(`${folderPath}`)) {
                 fs.mkdirSync(`${folderPath}`)
             }
             let extension = image_extension ? image_extension : 'png'
             let name = image_name ? `${image_name}.${extension}` : `${Date.now()}.${extension}`
+            result.image_name = `${folderPath}/${name}`.split('/screenshots/')[1]
+            if (fs.existsSync(`${folderPath}/${name}`)) {
+                result.error = 'Unique image name is required.'
+            }
             await page.screenshot({ path:`${folderPath}/${name}` });
             let closeTrigger = browser_close ? browser_close : true
             if(closeTrigger) {
                 await browser.close();
+                result.browser_closed = "true"
+            }else {
+                result.browser_closed = "false"
             }
+            return result
         }catch (e) {
             console.log(e)
+            return e
         }
     }
 }
--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const app = express()
 const take = require('./screenshot')
 
 app.post('/', bodyParser.json(), async function (req, res) {
-    if (!req.body.url) return res.status(401).json({status: 'error' , message: 'Url is required.'})
-    await take.screenshot(req.body.url, req.body.width, req.body.height, req.body.image_name, req.body.image_extension, req.body.browser_close)
-    return res.status(200).json({ status: 'success' })
+    try {
+        if (!req.body.url) return res.status(401).json({status: 'error' , message: 'Url is required.'})
+        const result = await take.screenshot(req.body.url, req.body.width, req.body.height, req.body.image_name, req.body.image_extension, req.body.browser_close)
+        if(result.error) {
+            return res.status(401).json({status: 'error' , message: result.error})
+        }
+        return res.status(200).json({ status: 'success', data: result })
+    }catch (e) {
+        console.log(e)
+        return res.status(500).json({ status: 'error' })
+
+    }
 })
 
 app.listen(process.env.POST, `${process.env.HOST}`,function () {
